3b153ee4a7ce4510bd72e2394d188482d30920a8
CleSucre <47931504+CleSucre@users.noreply.github.com>
2024-10-25 04:52:06 +0200

    fix pipes unclosed on empty command

--- src/execution/execution_pipes.c
+++ src/execution/execution_pipes.c
@@ -29,9 +29,15 @@ int	execute_pipe(t_minishell *minishell, t_ast_node *ast,
 	ast->left->in_pipe = 1;
 	ast->right->in_pipe = 1;
 	res = execute_ast(minishell, ast->left, pipes, in_out);
+	ft_fprintf(2, "res = %d\n", res);
 	if (res == 1)
 		return (res);
-	if (ast->right->type == AST_REDIR_IN || ast->right->type == AST_HEREDOC)
+	add_fd_to_close(minishell->opened_fds, pipes[0]);
+	add_fd_to_close(minishell->opened_fds, pipes[1]);
+	ft_fprintf(2, "value = %s\n", ast->left->value[0]);
+	if (!ast->left->value[0]
+		|| ast->right->type == AST_REDIR_IN
+		|| ast->right->type == AST_HEREDOC)
 	{
 		close_fds(in_out, pipes);
 		in_out[0] = STDIN_FILENO;
